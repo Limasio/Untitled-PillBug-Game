2ca8c3ece3b342cbd4458826438ea02380cb7090
Limasio <35857078+Limasio@users.noreply.github.com>
2024-08-19 01:37:45 -0500

    adding timer extenders and stuff

--- Assets/Scripts/Level/PlatformManager.cs
+++ Assets/Scripts/Level/PlatformManager.cs
@@ -5,52 +5,75 @@ using UnityEngine;
 
 public class PlatformManager : MonoBehaviour
 {
-    [SerializeField] Transform plat1;
-    [SerializeField] Transform plat2;
-    [SerializeField] Transform plat3;
+    [SerializeField] GameObject plat1;
+    [SerializeField] GameObject plat2;
+    [SerializeField] GameObject plat3;
+    [SerializeField] GameObject plat4;
     [SerializeField] GameObject player;
     //[SerializeField] Transform plat3;
 
     [SerializeField] int numberOfObjects;
+    [SerializeField] int maxTimers;
+    [SerializeField] int minTimerSpacing;
+    private int currentTimers;
+    private int timerSpacingCounter;
     [SerializeField] float recycleOffset;
     [SerializeField] Vector2 startPosition;
     [SerializeField] Vector2 minSize, maxSize, minGap, maxGap;
     [SerializeField] float minY, maxY;
 
     private Vector2 nextPosition;
-    private Queue<Transform> objectQueue;
+    private Queue<GameObject> objectQueue;
+
+    private void Awake()
+    {
+        currentTimers = 0;
+        timerSpacingCounter = minTimerSpacing;
+    }
 
     private void Start()
-    {
-        objectQueue = new Queue<Transform>(numberOfObjects);
+    { 
+        objectQueue = new Queue<GameObject>(numberOfObjects);
         for (int i = 0; i < numberOfObjects; i++)
         {
             float randomNumber = Random.Range(0.0f, 100.0f);
             if(randomNumber <= 33.3f)
             {
-                objectQueue.Enqueue((Transform)Instantiate(plat1));
+                objectQueue.Enqueue((GameObject)Instantiate(plat1));
             }
             else if (randomNumber <= 66.6f)
             {
-                objectQueue.Enqueue((Transform)Instantiate(plat2));
+                objectQueue.Enqueue((GameObject)Instantiate(plat2));
+            }
+            else if (randomNumber <= 77.7f && currentTimers < maxTimers && timerSpacingCounter == minTimerSpacing)
+            {
+                objectQueue.Enqueue((GameObject)Instantiate(plat4));
+                currentTimers++;
+                timerSpacingCounter = 0;
             }
             else
             {
-                objectQueue.Enqueue((Transform)Instantiate(plat3));
-            } 
+                objectQueue.Enqueue((GameObject)Instantiate(plat3));
+            }
+            if(timerSpacingCounter != minTimerSpacing)
+            {
+                timerSpacingCounter++;
+            }
         }
         nextPosition = startPosition;
         for (int i = 0; i < numberOfObjects; i++)
         {
             Recycle();
         }
+        currentTimers = 0;
     }
 
     private void Update()
     {
-        if(objectQueue.Peek().localPosition.x + recycleOffset < player.transform.localPosition.x) //Checking against total distance traveled by player
+        if(objectQueue.Peek().transform.localPosition.x + recycleOffset < player.transform.localPosition.x) //Checking against total distance traveled by player
         {
-            Recycle();
+            Recycle2();
+            Debug.Log("Ran Recycle2");
         }
     }
 
@@ -61,10 +84,11 @@ public class PlatformManager : MonoBehaviour
         position.x += scale.x * 0.5f;
         position.y += scale.y * 0.5f;
 
-        Transform o = objectQueue.Dequeue();
-        o.localScale = scale;
-        o.localPosition = position;
+        GameObject o = objectQueue.Dequeue();
+        o.transform.localScale = scale;
+        o.transform.localPosition = position;
         objectQueue.Enqueue(o);
+
 
         //nextPosition += new Vector2(Random.Range(minGap.x, maxGap.x) + scale.x,
         //    Random.Range(minGap.y, maxGap.y));
@@ -76,6 +100,55 @@ public class PlatformManager : MonoBehaviour
             nextPosition.y = minY + maxGap.y;
         }
         else if(nextPosition.y > maxY)
+        {
+            nextPosition.y = maxY - maxGap.y;
+        }
+    }
+
+    private void Recycle2()
+    {
+        objectQueue.Dequeue();
+
+        Vector2 scale = new Vector2(Random.Range(minSize.x, maxSize.x), Random.Range(minSize.y, maxSize.y));
+        Vector2 position = nextPosition;
+        position.x += scale.x * 0.5f;
+        position.y += scale.y * 0.5f;
+
+        float randomNumber = Random.Range(0.0f, 100.0f);
+        if (randomNumber <= 33.3f)
+        {
+            GameObject platClone = Instantiate(plat1, position, Quaternion.identity);
+            objectQueue.Enqueue(platClone);
+        }
+        else if (randomNumber <= 66.6f)
+        {
+            GameObject platClone = Instantiate(plat2, position, Quaternion.identity);
+            objectQueue.Enqueue(platClone);
+        }
+        else if (randomNumber <= 77.7f && currentTimers < maxTimers && timerSpacingCounter == minTimerSpacing)
+        {
+            GameObject platClone = Instantiate(plat4, position, Quaternion.identity);
+            objectQueue.Enqueue(platClone);
+            //currentTimers++;
+            timerSpacingCounter = 0;
+        }
+        else
+        {
+            GameObject platClone = Instantiate(plat3, position, Quaternion.identity);
+            objectQueue.Enqueue(platClone);
+        }
+        if (timerSpacingCounter != minTimerSpacing)
+        {
+            timerSpacingCounter++;
+        }
+
+        nextPosition += new Vector2(Random.Range(minGap.x, maxGap.x),
+            Random.Range(minGap.y, maxGap.y));
+        if (nextPosition.y < minY)
+        {
+            nextPosition.y = minY + maxGap.y;
+        }
+        else if (nextPosition.y > maxY)
         {
             nextPosition.y = maxY - maxGap.y;
         }
